26d32be06a8ca1a9e18628c119e4456f8470ec4f
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2024-03-05 22:23:24 +0500

    generate Token

--- backend/helpers/token.js
+++ backend/helpers/token.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 class TokenHelper {
-    
+    async generateToken(payload){
+       const token = jwt.sign(payload, process.env.JWTSECRET, {expiresIn: '1h'} );
+       return token;
+    }
 }
